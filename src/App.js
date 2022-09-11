7c916c6c4ef53316ca5775c5a43af700ce65c5b1
hk8522 <94052790+hk8522@users.noreply.github.com>
2022-09-11 07:30:56 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, {Suspense, Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {useTranslation} from "react-i18next";
 import { withTranslation } from 'react-i18next';
 
 function HeaderComponent()
 {
-    // const {t, i18n} = useTranslation('common');
-    // return <h1>{t('welcome.title')}</h1>
+  // const {t, i18n} = useTranslation('common');
+  // return <h1>{t('welcome.title')}</h1>
 
-    const [t, i18n] = useTranslation('common');
-    return <div>
-        <h1>{t('welcome.title', {framework:'React'})}</h1>
-        <button onClick={() => i18n.changeLanguage('de')}>de</button>
-        <button onClick={() => i18n.changeLanguage('en')}>en</button>
-    </div>
+  const [t, i18n] = useTranslation('common');
+  return <div>
+    <h1>{t('welcome.title', {framework:'React'})}</h1>
+    <button onClick={() => i18n.changeLanguage('de')}>de</button>
+    <button onClick={() => i18n.changeLanguage('en')}>en</button>
+  </div>
 }
 
 class HighOrderComponent extends Component {
   render() {
-      const { t } = this.props;
+    const { t } = this.props;
 
-      return (<div>
-          <h1>{t('welcome.title')}</h1>
-          <h1>{t('welcome.title', {framework:'React'})}</h1>
-        </div>
-      )
+    return (<div>
+        <h1>{t('welcome.title')}</h1>
+        <h1>{t('welcome.title', {framework:'React'})}</h1>
+      </div>
+    )
   }
 }
 const HighOrderComponentTranslated = withTranslation('common')(HighOrderComponent)
@@ -38,24 +37,6 @@ function App() {
       </div>
     </Suspense>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
