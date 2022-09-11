052a6d0812c5ec641a652df6f4e908a0711b72f7
hk8522 <94052790+hk8522@users.noreply.github.com>
2022-09-11 07:43:33 +0300

    format

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, {Suspense, Component} from 'react';
-import './App.css';
-import {useTranslation} from "react-i18next";
-import { withTranslation } from 'react-i18next';
+import React, { Suspense, Component } from "react";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import { withTranslation } from "react-i18next";
 
-function HeaderComponent()
-{
+function HeaderComponent() {
   // const {t, i18n} = useTranslation('common');
   // return <h1>{t('welcome.title')}</h1>
 
-  const [t, i18n] = useTranslation('common');
-  return <div>
-    <h1>{t('welcome.title', {framework:'React'})}</h1>
-    <button onClick={() => i18n.changeLanguage('de')}>de</button>
-    <button onClick={() => i18n.changeLanguage('en')}>en</button>
-  </div>
+  const [t, i18n] = useTranslation("common");
+  return (
+    <div>
+      <h1>{t("welcome.title", { framework: "React" })}</h1>
+      <button onClick={() => i18n.changeLanguage("de")}>de</button>
+      <button onClick={() => i18n.changeLanguage("en")}>en</button>
+    </div>
+  );
 }
 
 class HighOrderComponent extends Component {
   render() {
     const { t } = this.props;
 
-    return (<div>
-        <h1>{t('welcome.title')}</h1>
-        <h1>{t('welcome.title', {framework:'React'})}</h1>
+    return (
+      <div>
+        <h1>{t("welcome.title")}</h1>
+        <h1>{t("welcome.title", { framework: "React" })}</h1>
       </div>
-    )
+    );
   }
 }
-const HighOrderComponentTranslated = withTranslation('common')(HighOrderComponent)
+const HighOrderComponentTranslated =
+  withTranslation("common")(HighOrderComponent);
 
 function App() {
   return (
     <Suspense fallback="loading">
       <div className="App">
-        <HeaderComponent/>
+        <HeaderComponent />
       </div>
     </Suspense>
   );
